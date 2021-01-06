aa1f3278e604904e62c2287004b35f7aa20ddd33
Patrick Chen <patrick.chen@higgstar.com>
2021-01-06 10:14:05 +0800

    APP-977 [Register] 註冊基本資料填寫手機號碼的欄位，未帶出 CNY 手機區碼 +86 1. 修正遺失的nameEdited 2. 修改testfield 3. 重串修改後的textfield

--- ktobet-asia-ios/presention/viewController/signup/SignupUserInfoViewModel.swift
+++ ktobet-asia-ios/presention/viewController/signup/SignupUserInfoViewModel.swift
@@ -20,6 +20,7 @@ class SignupUserInfoViewModel {
     
     enum UserInfoStatus{
         case valid
+        case firstEmpty
         case empty
         case errNameFormat
         case errEmailFormat
@@ -33,33 +34,22 @@ class SignupUserInfoViewModel {
     private var usecaseRegister : IRegisterUseCase!
     private var usecaseSystemStatus : GetSystemStatusUseCase!
     
-    private var relayName = BehaviorRelay(value: "")
-    private var relayAccunt = BehaviorRelay(value: "")
-    private var relayPassword = BehaviorRelay(value: "")
-    private var relayConfirmPassword = BehaviorRelay(value: "")
-    private var relayAccountType = BehaviorRelay(value: AccountType.phone)
+    private var phoneEdited = false
+    private var mailEdited = false
+    private var passwordEdited = false
+    private var nameEdited = false
     
+    var relayName = BehaviorRelay(value: "")
+    var relayEmail = BehaviorRelay(value: "")
+    var relayMobile = BehaviorRelay(value: "")
+    var relayPassword = BehaviorRelay(value: "")
+    var relayConfirmPassword = BehaviorRelay(value: "")
+    var relayAccountType = BehaviorRelay(value: AccountType.phone)
     var locale : SupportLocale = SupportLocale.China()
     
     init(_ usecaseRegister : IRegisterUseCase, _ usecaseSystem : GetSystemStatusUseCase) {
         self.usecaseRegister = usecaseRegister
         self.usecaseSystemStatus = usecaseSystem
-    }
-    
-    func inputName(_ name : String){
-        relayName.accept(name)
-    }
-    
-    func inputAccount(_ account : String){
-        relayAccunt.accept(account)
-    }
-    
-    func inputPassword(_ password: String){
-        relayPassword.accept(password)
-    }
-    
-    func inputConfirmPassword(_ confirmPassword : String){
-        relayConfirmPassword.accept(confirmPassword)
     }
     
     func inputAccountType(_ type: AccountType){
@@ -74,17 +64,14 @@ class SignupUserInfoViewModel {
         return relayAccountType.value
     }
     
-    func currentAccount()->String{
-        return relayAccunt.value
-    }
-    
     func currentPassword()->String{
         return relayPassword.value
     }
     
     func event()->(otpValid : Observable<UserInfoStatus>,
+                   emailValid : Observable<UserInfoStatus>,
+                   mobileValid : Observable<UserInfoStatus>,
                    nameValid : Observable<UserInfoStatus>,
-                   accountValid : Observable<UserInfoStatus>,
                    passwordValid : Observable<UserInfoStatus>,
                    dataValid : Observable<Bool>,
                    typeChange : Observable<AccountType>){
@@ -92,30 +79,40 @@ class SignupUserInfoViewModel {
         let nameValid = relayName
             .map { (text) -> UserInfoStatus in
                 let valid = self.locale.verifyWithdrawalNameFormat(name: text)
+                if text.count > 0 { self.nameEdited = true }
                 if valid { return .valid }
-                else if text.count == 0 { return .empty}
-                else { return .errNameFormat}
-            }
-        
-        let accountValid = relayAccunt
-            .map { (text) -> UserInfoStatus in
-                let type = self.relayAccountType.value
-                let valid : Bool = {
-                    switch type {
-                    case .email: return Account.Email(email: text).isValid()
-                    case .phone: return Account.Phone(phone: text, locale: self.locale).isValid()
-                    }
-                }()
-                if valid { return .valid }
-                else if text.count == 0 { return .empty }
-                else {
-                    switch type{
-                    case .email: return .errEmailFormat
-                    case .phone: return .errPhoneFormat
-                    }
+                else if text.count == 0{
+                    if self.nameEdited { return .empty}
+                    else { return .firstEmpty }
+                } else {
+                    return .errNameFormat
                 }
             }
-
+        
+        let emailValid = relayEmail
+            .map { (text) -> UserInfoStatus in
+                let valid = Account.Email(email: text).isValid()
+                if text.count > 0 { self.mailEdited = true }
+                if valid { return .valid}
+                else if text.count == 0 {
+                    if self.mailEdited{ return .empty }
+                    else { return .firstEmpty }
+                }
+                else { return .errEmailFormat}
+            }
+        
+        let mobileValid = relayMobile
+            .map { (text) -> UserInfoStatus in
+                let valid = Account.Phone(phone: text, locale: self.locale).isValid()
+                if text.count > 0 { self.phoneEdited = true }
+                if valid { return .valid}
+                else if text.count == 0 {
+                    if self.phoneEdited { return .empty }
+                    else { return .firstEmpty }
+                }
+                else { return .errPhoneFormat}
+            }
+        
         
         let password = relayPassword.asObservable()
         let confirmPassword = relayConfirmPassword.asObservable()
@@ -123,8 +120,10 @@ class SignupUserInfoViewModel {
             .flatMapLatest { (passwordText)  in
                 return confirmPassword.map { (confirmPasswordText) -> UserInfoStatus in
                     let valid = UserPassword.Companion().verify(password: passwordText)
+                    if passwordText.count > 0 { self.passwordEdited = true }
                     if passwordText.count == 0{
-                        return .empty
+                        if self.passwordEdited { return .empty }
+                        else { return .firstEmpty }
                     } else if (!valid){
                         return .errPasswordFormat
                     } else if passwordText != confirmPasswordText{
@@ -135,12 +134,17 @@ class SignupUserInfoViewModel {
                 }
             }
         
+        let typeChange = relayAccountType.asObservable()
+        
+        let accountValid = Observable.combineLatest(typeChange, emailValid, mobileValid){
+            ($0 == AccountType.email && $1 == UserInfoStatus.valid) ||
+            ($0 == AccountType.phone && $2 == UserInfoStatus.valid)
+        }
+    
         let dataValid = Observable
             .combineLatest(nameValid, accountValid, passwordValid){
-                (($0 == UserInfoStatus.valid) && ($1 == UserInfoStatus.valid) && ($2 == UserInfoStatus.valid))
+                (($0 == UserInfoStatus.valid) && $1 && ($2 == UserInfoStatus.valid))
             }
-        
-        let typeChange = relayAccountType.asObservable()
         
         let otpValid = usecaseSystemStatus
             .getOtpStatus()
@@ -153,23 +157,40 @@ class SignupUserInfoViewModel {
                     }
                 }
             }
+        
         return (otpValid : otpValid,
+                emailValid : emailValid,
+                mobileValid : mobileValid,
                 nameValid : nameValid,
-                accountValid : accountValid,
                 passwordValid : passwordValid,
                 dataValid : dataValid,
                 typeChange : typeChange)
     }
     
 
-    func register()->Completable  {
+    func register()->Single<(type: AccountType, account: String, password: String)>  {
         let userAccount : UserAccount = {
             switch relayAccountType.value{
-            case .phone: return UserAccount(username: relayName.value, type: Account.Phone(phone: relayAccunt.value, locale: locale))
-            case .email: return UserAccount(username: relayName.value, type: Account.Email(email: relayAccunt.value))
+            case .phone:
+                return UserAccount(username: relayName.value, type: Account.Phone(phone: relayMobile.value, locale: locale))
+            case .email:
+                return UserAccount(username: relayName.value, type: Account.Email(email: relayEmail.value))
             }
         }()
         let userPassword = UserPassword(value: relayPassword.value)
-        return usecaseRegister.register(account: userAccount, password: userPassword, locale: self.locale)
+        let nextAction =  Single<(type: AccountType, account: String, password: String)>
+            .create { (single) -> Disposable in
+                var account = ""
+                let password = self.relayPassword.value
+                switch self.relayAccountType.value{
+                case .phone: account = self.relayMobile.value
+                case .email: account = self.relayEmail.value
+                }
+                single(.success((self.relayAccountType.value, account, password)))
+                return Disposables.create {}
+            }
+        return usecaseRegister
+            .register(account: userAccount, password: userPassword, locale: self.locale)
+            .andThen(nextAction)
     }
 }
