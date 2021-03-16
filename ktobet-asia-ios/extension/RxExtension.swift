47509c64530c032976f6b5ae3f49e8e0fe905f54
Leo Hsu <leo.hsu@higgstar.com>
2021-03-16 16:24:15 +0800

    Error handle

---
+++ ktobet-asia-ios/extension/RxExtension.swift
@@ -0,0 +1,110 @@
+//
+//  RxExtension.swift
+//  ktobet-asia-ios
+//
+//  Created by Leo Hsu on 2021/3/11.
+//
+
+import RxSwift
+import RxCocoa
+
+///reference: https://gist.github.com/sgr-ksmt/c05cc1cf41a32b8b77e277394c29509a
+struct ObservableTransformer<T, R> {
+    let transformer: (Observable<T>) -> Observable<R>
+    init(transformer: @escaping (Observable<T>) -> Observable<R>) {
+        self.transformer = transformer
+    }
+    
+    func call(_ observable: Observable<T>) -> Observable<R> {
+        return transformer(observable)
+    }
+}
+
+extension ObservableType {
+    func compose<T>(_ transformer: ObservableTransformer<Element, T>) -> Observable<T> {
+        return transformer.call(self.asObservable())
+    }
+    ///reference: https://github.com/ReactiveX/RxSwift/blob/c4e4c1bd1c098ab942f0dc42b4fd66ab62e1bf9e/RxSwift/Observables/WithUnretained.swift
+    /**
+     Provides an unretained, safe to use (i.e. not implicitly unwrapped), reference to an object along with the events emitted by the sequence.
+     
+     In the case the provided object cannot be retained successfully, the seqeunce will complete.
+     
+     - note: Be careful when using this operator in a sequence that has a buffer or replay, for example `share(replay: 1)`, as the sharing buffer will also include the provided object, which could potentially cause a retain cycle.
+     
+     - parameter obj: The object to provide an unretained reference on.
+     - parameter resultSelector: A function to combine the unretained referenced on `obj` and the value of the observable sequence.
+     - returns: An observable sequence that contains the result of `resultSelector` being called with an unretained reference on `obj` and the values of the original sequence.
+     */
+    public func withUnretained<Object: AnyObject, Out>(
+        _ obj: Object,
+        resultSelector: @escaping (Object, Element) -> Out
+    ) -> Observable<Out> {
+        map { [weak obj] element -> Out in
+            guard let obj = obj else { throw UnretainedError.failedRetaining }
+
+            return resultSelector(obj, element)
+        }
+        .catchError{ error -> Observable<Out> in
+            guard let unretainedError = error as? UnretainedError,
+                  unretainedError == .failedRetaining else {
+                return .error(error)
+            }
+
+            return .empty()
+        }
+    }
+
+    
+    /**
+     Provides an unretained, safe to use (i.e. not implicitly unwrapped), reference to an object along with the events emitted by the sequence.
+     
+     In the case the provided object cannot be retained successfully, the seqeunce will complete.
+     
+     - note: Be careful when using this operator in a sequence that has a buffer or replay, for example `share(replay: 1)`, as the sharing buffer will also include the provided object, which could potentially cause a retain cycle.
+     
+     - parameter obj: The object to provide an unretained reference on.
+     - returns: An observable sequence of tuples that contains both an unretained reference on `obj` and the values of the original sequence.
+     */
+    public func withUnretained<Object: AnyObject>(_ obj: Object) -> Observable<(Object, Element)> {
+        return withUnretained(obj) { ($0, $1) }
+    }
+}
+
+struct SingleTransformer<T, R> {
+    let transformer: (Single<T>) -> Single<R>
+    init(transformer: @escaping (Single<T>) -> Single<R>) {
+        self.transformer = transformer
+    }
+    
+    func call(_ single: Single<T>) -> Single<R> {
+        return transformer(single)
+    }
+}
+
+extension Single where PrimitiveSequence.Trait == RxSwift.SingleTrait {
+    func compose<T>(_ transformer: SingleTransformer<Element, T>) -> Single<T> {
+        return transformer.call(self)
+    }
+}
+
+struct CompletableTransformer {
+    let transformer: (Completable) -> Completable
+    init(transformer: @escaping (Completable) -> Completable) {
+        self.transformer = transformer
+    }
+    
+    func call(_ completable: Completable) -> Completable {
+        return transformer(completable)
+    }
+}
+
+extension Completable {
+    func compose(_ transformer: CompletableTransformer) -> Completable {
+        return transformer.call(self)
+    }
+}
+
+private enum UnretainedError: Swift.Error {
+    case failedRetaining
+}
---
+++ ktobet-asia-ios/presention/viewController/MainDebugViewController.swift
@@ -0,0 +1,106 @@
+//
+//  MainDebugViewController.swift
+//  ktobet-asia-ios
+//
+//  Created by LeoOnHiggstar on 2021/3/15.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+struct DebugData {
+    var callbackTime: String?
+    var url: String?
+    var headers: String?
+    var body: String?
+    var error: String?
+    var response: String?
+}
+
+class MainDebugViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    @IBOutlet weak var itemViewHeight: NSLayoutConstraint!
+    var cancelHandle: (() -> Void)?
+    
+    private var debugDatas = BehaviorRelay<[DebugData]>(value: [])
+    private var debugLogs: [String] = []
+
+    private lazy var disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.setupBinding()
+        loadData(DIContainer.share.httpClient.debugDatas.reversed())
+    }
+    
+    private func setCell(cell:MainDebugDataCell, callbackTime: String?, url: String?, headers: String?, body: String?, error: String?, response: String?) {
+        
+        cell.callbackTimeRowStackView.isHidden = callbackTime == nil
+        cell.urlRowStackView.isHidden = url == nil
+        cell.headersRowStackView.isHidden = headers == nil
+        cell.bodyRowStackView.isHidden = body == nil
+        
+        cell.errorImageView.isHidden = error == nil
+
+        cell.callbackTimeLabel.text = callbackTime
+        cell.urlLabel.text = url
+        cell.headersLabel.text = headers
+        cell.responseLabel.textColor = error != nil ? UIColor(hex: 0xff0000) : UIColor(hex: 0x000000)
+        cell.responseLabel.text = error != nil ? error : response
+        cell.bodyLabel.text = body
+        
+    }
+
+    private func setupBinding() {
+        self.debugDatas.asDriver().drive(tableView.rx.items(cellIdentifier: "MainDebugDataCell", cellType: MainDebugDataCell.self)) { [weak self] row, item, cell in
+            self?.setCell(cell: cell, callbackTime: item.callbackTime, url: item.url, headers: item.headers, body: item.body, error: item.error, response: item.response)
+        }.disposed(by: self.disposeBag)
+        
+        self.tableView.rx.itemSelected.subscribe(onNext: { [weak self] indexPath in
+            self?.tableView.deselectRow(at: indexPath, animated: true)
+            guard !(self?.debugLogs.isEmpty ?? true), let log = self?.debugLogs[indexPath.row] else { return }
+            self?.presentActivityView(with: [log])
+        }).disposed(by: self.disposeBag)
+        
+        self.cancelButton.rx.touchUpInside.subscribe(onNext: { [weak self] _ in
+            self?.cancelHandle?()
+        }).disposed(by: self.disposeBag)
+    }
+    
+    private func loadData(_ logs: [DebugData]) {
+        debugDatas.accept(logs)
+    }
+    
+    private func presentActivityView(with shareItem: [Any],
+                             excluded types: [UIActivity.ActivityType]? = nil,
+                             completionWithItemsHandler: UIActivityViewController.CompletionWithItemsHandler? = nil) {
+        let activityViewController = UIActivityViewController(activityItems: shareItem, applicationActivities: nil)
+        activityViewController.excludedActivityTypes = types
+        activityViewController.completionWithItemsHandler = completionWithItemsHandler
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    deinit {
+        print("\(type(of: self)) deinit")
+    }
+}
+
+class MainDebugDataCell: UITableViewCell {
+    @IBOutlet weak var callbackTimeRowStackView: UIStackView!
+    @IBOutlet weak var urlRowStackView: UIStackView!
+    @IBOutlet weak var headersRowStackView: UIStackView!
+    @IBOutlet weak var bodyRowStackView: UIStackView!
+    @IBOutlet weak var responseRowStackView: UIStackView!
+    
+    @IBOutlet weak var errorImageView: UIImageView!
+    @IBOutlet weak var callbackTimeLabel: UILabel!
+    @IBOutlet weak var urlLabel: UILabel!
+    @IBOutlet weak var headersLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var responseLabel: UILabel!
+}
