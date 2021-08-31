6780bbdbba3d42af6e1843081e86cbf466a86672
weichen Cheng <weichen.cheng@higgstar.com>
2021-08-31 11:10:44 +0800

    APP-2106 APP-2107 APP-2108 APP-2109 APP-2111 APP-2110 APP-2106 [iOS][Level Privilege] 存送特權 明細頁，項目標題文字錯誤 APP-2107 [iOS][Level Privilege] 存送特權 明細頁，"存送规则" 項目標題文字不應顯示為 "每月有奖" APP-2108 [iOS][Level Privilege] 存送特權 LV1、LV2 明細頁，最低帳戶餘額不需顯示小數 ".0" APP-2109 [iOS][Level Privilege] 存送特權 明細頁，存送規則 "(本金+紅利)" 應改為 "(本金+彩金)" APP-2111 [iOS][Level Privilege] LV1 提現特權 提現總限額顯示小數末位為 0 時不需顯示 APP-2110 [iOS][Level Privilege] 特權明細頁點擊 "前往優惠專區" 無作用

--- ktobet-asia-ios/presention/viewController/level/LevelPrivilegeDetailViewController.swift
+++ ktobet-asia-ios/presention/viewController/level/LevelPrivilegeDetailViewController.swift
@@ -23,6 +23,8 @@ class LevelPrivilegeDetailViewController: UIViewController {
     var levelPrivilege: LevelPrivilege!
     var level: Int32!
     private var arg: PrivilegeArg!
+    private var cells: [UITableViewCell] = []
+    private var disposeBag: DisposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +33,9 @@ class LevelPrivilegeDetailViewController: UIViewController {
         NavigationManagement.sharedInstance.addBackToBarButtonItem(vc: self, image: "iconNavBack24")
         
         btnPromotion.applyGradient(horizontal: [UIColor.yellowFull.cgColor, UIColor(red: 254/255, green: 161/255, blue: 68/255, alpha: 1).cgColor])
+        btnPromotion.rx.touchUpInside.subscribe(onNext:{
+            NavigationManagement.sharedInstance.goTo(storyboard: "Game", viewControllerId: "AccountGiftNavigationController")
+        }).disposed(by: disposeBag)
         backgroundView.setViewCorner(topCorner: true, bottomCorner: false, radius: 32)
         levelLabel.layer.masksToBounds = true
         levelLabel.layer.cornerRadius = 16
@@ -39,14 +44,14 @@ class LevelPrivilegeDetailViewController: UIViewController {
         
         levelLabel.text = String(format: Localize.string("common_level_2"), String(level))
         switch levelPrivilege {
-        case is LevelPrivilege.Deposit:
-            arg = generateDepositView()
-        case is LevelPrivilege.Rebate:
-            arg = generateProductFeedback()
+        case let depositData as LevelPrivilege.Deposit:
+            generateDepositView(data: depositData)
+        case let rebateData as LevelPrivilege.Rebate:
+            generateProductFeedback(data: rebateData)
         case is LevelPrivilege.ProductSlotRescue:
-            arg = generateSlot()
-        case is LevelPrivilege.ProductBetInsurance:
-            arg = generateInsureance()
+            generateSlot()
+        case let insuranceData as LevelPrivilege.ProductBetInsurance:
+            generateInsureance(data: insuranceData)
         default:
             break
         }
@@ -54,119 +59,69 @@ class LevelPrivilegeDetailViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         // Make the navigation bar background clear
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         navigationController?.navigationBar.isTranslucent = true
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
+        
         // Restore the navigation bar to default
         navigationController?.navigationBar.setBackgroundImage(nil, for: .default)
         navigationController?.navigationBar.shadowImage = nil
     }
     
-    private func generateDepositView() -> PrivilegeArg {
+    private func generateDepositView(data: LevelPrivilege.Deposit) {
         titleLabel.text = Localize.string("common_depositbonus")
         iconImageView.image = UIImage(named: "lvDetailBank")
-        var cells: [UITableViewCell] = []
-        if let data = levelPrivilege as? LevelPrivilege.Deposit {
-            for row in 0..<(level >= 3 ? 4 : 3) {
-                let detailCell = tableView.dequeueReusableCell(withIdentifier: "LevelDetailTableViewCell") as! LevelDetailTableViewCell
-                let detail2Cell = tableView.dequeueReusableCell(withIdentifier: "LevelDetail2TableViewCell") as! LevelDetail2TableViewCell
-                
-                if row == 1 {
-                    detailCell.leftLabel.text = Localize.string("level_detail_5_title1")
-                    detailCell.rightLabel.text = data.maxBonus.amount.currencyFormatWithoutSymbol(precision: 0)
-                    cells.append(detailCell)
-                } else if row == 0 {
-                    detailCell.leftLabel.text = Localize.string("level_detail_5_title1_first")
-                    detailCell.rightLabel.text = data.percentage.description() + "%"
-                    cells.append(detailCell)
-                } else if row == 2 {
-                    if level <= 2 {
-                        detail2Cell.titleLabel.text = Localize.string("level_detail_5_title3")
-                        detail2Cell.secondLabel.text = String(format: Localize.string("level_detail_5_content3"), data.minCapital.description(), String(data.betMultiple))
-                    } else {
-                        switch data.issueFrequency {
-                        case .daily:
-                            detail2Cell.titleLabel.text = Localize.string("level_detail_5_title2_3")
-                            detail2Cell.secondLabel.text = Localize.string("level_detail_5_content2_3")
-                        case .weekly:
-                            detail2Cell.titleLabel.text = Localize.string("level_detail_5_title2_2")
-                            detail2Cell.secondLabel.text = Localize.string("level_detail_5_content2_2")
-                        case .monthly:
-                            detail2Cell.titleLabel.text = Localize.string("level_detail_5_title2_1")
-                            detail2Cell.secondLabel.text = Localize.string("level_detail_5_content2_1")
-                        default:
-                            break
-                        }
-                    }
-                    
-                    cells.append(detail2Cell)
-                } else if row == 3 {
-                    if level >= 3 {
-                        detail2Cell.titleLabel.text = Localize.string("level_detail_5_title2_1")
-                        detail2Cell.secondLabel.text = String(format: Localize.string("level_detail_5_content3"), data.minCapital.description(), String(data.betMultiple))
-                    }
-                    
-                    cells.append(detail2Cell)
-                }
-            }
+        if (1...2).contains(level) {
+            cells = generateDepositLevelOneTwo(data: data)
+        } else if (3...10).contains(level) {
+            cells = generateDepositGreaterThanLevelTwo(data: data)
         }
         
-        cells.append(UITableViewCell())
-        return PrivilegeArg(cells: cells, rowCount: cells.count)
+        cells.last?.addBorder(.bottom, size: 0.5, color: .dividerCapeCodGray2, rightConstant: 40, leftConstant: 40)
+        arg = PrivilegeArg(cells: cells, rowCount: cells.count)
     }
     
-    private func generateProductFeedback() -> PrivilegeArg {
+    private func generateProductFeedback(data: LevelPrivilege.Rebate) {
         titleLabel.text = Localize.string("level_product_rebate")
         iconImageView.image = UIImage(named: "lvDetailProduct")
         footerView.isHidden = false
-        var cells: [UITableViewCell] = []
-        if let data = levelPrivilege as? LevelPrivilege.Rebate {
-            dailyLimitAmountLabel.text = data.maxBonus.amount.currencyFormatWithoutSymbol(precision: 0)
-            let titles = [Localize.string("common_sportsbook"), Localize.string("common_casino"), Localize.string("common_slot"), Localize.string("common_keno")]
-            let content: [String] =
-                [data.percentages[ProductType.sbk]?.description() ?? "" + "%",
-                 data.percentages[ProductType.casino]?.description() ?? "" + "%",
-                 data.percentages[ProductType.slot]?.description() ?? "" + "%",
-                 data.percentages[ProductType.numbergame]?.description() ?? "" + "%"]
-                        
-            for i in 0...3 {
-                let detailCell = tableView.dequeueReusableCell(withIdentifier: "LevelDetailTableViewCell") as! LevelDetailTableViewCell
-                detailCell.leftLabel.text = titles[i]
-                detailCell.rightLabel.text = content[i]
-                cells.append(detailCell)
-            }
-            
-            cells.append(UITableViewCell())
-            
-            if !data.isMaxBonusLimited() {
-                productUnlimitedTopBarView.backgroundColor = UIColor.orangeFull
-                productUnlimitedView.backgroundColor = UIColor(red: 1, green: 128/255, blue: 0, alpha: 0.2)
-                dailyLimitAmountLabel.text = Localize.string("bonus_unlimited")
-            }
+        dailyLimitAmountLabel.text = data.maxBonus.description()
+        let titles = [Localize.string("common_sportsbook"), Localize.string("common_casino"), Localize.string("common_slot"), Localize.string("common_keno"), Localize.string("common_arcade")]
+        let content: [String] =
+            [data.percentages[ProductType.sbk]?.description() ?? "" + "%",
+             data.percentages[ProductType.casino]?.description() ?? "" + "%",
+             data.percentages[ProductType.slot]?.description() ?? "" + "%",
+             data.percentages[ProductType.numbergame]?.description() ?? "" + "%",
+             data.percentages[ProductType.arcade]?.description() ?? "" + "%"]
+        
+        for i in 0...4 {
+            cells.append(generateDetailOneRowCell(leftContent: titles[i], RightContent: content[i]))
         }
         
-        return PrivilegeArg(cells: cells, rowCount: cells.count)
+        if !data.isMaxBonusLimited() {
+            productUnlimitedTopBarView.backgroundColor = UIColor.orangeFull
+            productUnlimitedView.backgroundColor = UIColor(red: 1, green: 128/255, blue: 0, alpha: 0.2)
+            dailyLimitAmountLabel.text = Localize.string("bonus_unlimited")
+        }
+        
+        cells.last?.addBorder(.bottom, size: 0.5, color: .dividerCapeCodGray2, rightConstant: 40, leftConstant: 40)
+        arg = PrivilegeArg(cells: cells, rowCount: cells.count)
     }
     
-    private func generateSlot() -> PrivilegeArg {
+    private func generateSlot() {
         titleLabel.text = Localize.string("bonus_bonusproducttype_2")
         iconImageView.image = UIImage(named: "lvDetailSlot")
-        var cells: [UITableViewCell] = []
-        let detail2Cell = tableView.dequeueReusableCell(withIdentifier: "LevelDetail2TableViewCell") as! LevelDetail2TableViewCell
-        let imageCell = tableView.dequeueReusableCell(withIdentifier: "LevelDetailImageTableViewCell") as! LevelDetailImageTableViewCell
+        cells.append(generateDetailTwoRowCell(firstRow: Localize.string("level_detail_3_1_title"), secondRow: Localize.string("level_detail_3_1_content")))
         
-        detail2Cell.titleLabel.text = Localize.string("level_detail_3_1_title")
-        detail2Cell.secondLabel.text = Localize.string("level_detail_3_1_content")
-        cells.append(detail2Cell)
+        let imageCell = tableView.dequeueReusableCell(withIdentifier: "LevelDetailImageTableViewCell") as! LevelDetailImageTableViewCell
         if level >= 1 && level <= 4 {
-        imageCell.slotImageView.image = UIImage(named: "group1-4")
+            imageCell.slotImageView.image = UIImage(named: "group1-4")
         } else if level >= 5 && level <= 6 {
             imageCell.slotImageView.image = UIImage(named: "group5-6")
         } else if level >= 7 && level <= 8 {
@@ -176,28 +131,71 @@ class LevelPrivilegeDetailViewController: UIViewController {
         }
         
         cells.append(imageCell)
-        
-        return PrivilegeArg(cells: cells, rowCount: cells.count)
+        arg = PrivilegeArg(cells: cells, rowCount: cells.count)
     }
     
-    private func generateInsureance() -> PrivilegeArg {
+    private func generateInsureance(data: LevelPrivilege.ProductBetInsurance) {
         titleLabel.text = Localize.string("level_producttype_1")
         iconImageView.image = UIImage(named: "lvDetailInsure")
-        var cells: [UITableViewCell] = []
-        if let data = levelPrivilege as? LevelPrivilege.ProductBetInsurance {
-            let titles = [Localize.string("level_detail_3_1_title1"), Localize.string("level_detail_3_1_title2")]
-            let contents = [data.percentage.description(),
-                            data.maxBonus.description()]
-            for i in 0..<2 {
-                let detailCell = tableView.dequeueReusableCell(withIdentifier: "LevelDetailTableViewCell") as! LevelDetailTableViewCell
-                detailCell.leftLabel.text = titles[i]
-                detailCell.rightLabel.text = contents[i]
-                cells.append(detailCell)
-            }
+        let titles = [Localize.string("level_detail_3_1_title1"), Localize.string("level_detail_3_1_title2")]
+        let contents = [data.percentage.description(),
+                        data.maxBonus.description()]
+        for i in 0..<2 {
+            cells.append(generateDetailOneRowCell(leftContent: titles[i], RightContent: contents[i]))
         }
         
         cells.append(UITableViewCell())
-        return PrivilegeArg(cells: cells, rowCount: cells.count)
+        arg = PrivilegeArg(cells: cells, rowCount: cells.count)
+    }
+    
+    private func generateDepositLevelOneTwo(data: LevelPrivilege.Deposit) -> [UITableViewCell] {
+        cells.append(generateDetailOneRowCell(leftContent: level == 1 ? Localize.string("level_detail_5_title1_first") : Localize.string("level_detail_5_title1"), RightContent: data.percentage.description() + "%"))
+        cells.append(generateDetailOneRowCell(leftContent: Localize.string("level_detail_5_maxamount"),
+                                              RightContent: data.maxBonus.description()))
+        cells.append(generateDetailTwoRowCell(firstRow: Localize.string("level_detail_5_title3"),
+                                              secondRow: String(format: Localize.string("level_detail_5_content3"), data.minCapital.description(), String(data.betMultiple))))
+        
+        return cells
+    }
+    
+    private func generateDepositGreaterThanLevelTwo(data: LevelPrivilege.Deposit) -> [UITableViewCell] {
+        cells.append(generateDetailOneRowCell(leftContent: Localize.string("level_detail_5_title1"), RightContent: data.percentage.description() + "%"))
+        cells.append(generateDetailOneRowCell(leftContent: Localize.string("level_detail_5_maxamount"), RightContent: data.maxBonus.description()))
+        
+        var frequencyTitle = ""
+        var frequencyContent = ""
+        switch data.issueFrequency {
+        case .daily:
+            frequencyTitle = Localize.string("level_detail_5_title2_3")
+            frequencyContent = Localize.string("level_detail_5_content2_3")
+        case .weekly:
+            frequencyTitle = Localize.string("level_detail_5_title2_2")
+            frequencyContent = Localize.string("level_detail_5_content2_2")
+        case .monthly:
+            frequencyTitle = Localize.string("level_detail_5_title2_1")
+            frequencyContent = Localize.string("level_detail_5_content2_1")
+        default:
+            break
+        }
+        
+        cells.append(generateDetailTwoRowCell(firstRow: frequencyTitle, secondRow: frequencyContent))
+        cells.append(generateDetailTwoRowCell(firstRow: Localize.string("level_detail_5_title3"), secondRow: String(format: Localize.string("level_detail_5_content3"), data.minCapital.description(), String(data.betMultiple))))
+        
+        return cells
+    }
+    
+    private func generateDetailOneRowCell(leftContent: String, RightContent: String) -> UITableViewCell {
+        let detailCell = tableView.dequeueReusableCell(withIdentifier: "LevelDetailTableViewCell") as! LevelDetailTableViewCell
+        detailCell.leftLabel.text = leftContent
+        detailCell.rightLabel.text = RightContent
+        return detailCell
+    }
+    
+    private func generateDetailTwoRowCell(firstRow: String, secondRow: String) -> UITableViewCell {
+        let detail2RowCell = tableView.dequeueReusableCell(withIdentifier: "LevelDetail2TableViewCell") as! LevelDetail2TableViewCell
+        detail2RowCell.titleLabel.text = firstRow
+        detail2RowCell.secondLabel.text = secondRow
+        return detail2RowCell
     }
 }
 
