5af135498f09bd16453951d464c28439f5c961c2
LeoHsu <leo.hsu@higgstar.com>
2021-09-06 15:11:21 +0800

    [Fix] WebView with user-agent

--- ktobet-asia-ios/presention/viewController/lobby/Deposit/DepositThirdPartWebViewController.swift
+++ ktobet-asia-ios/presention/viewController/lobby/Deposit/DepositThirdPartWebViewController.swift
@@ -20,6 +20,8 @@ class DepositThirdPartWebViewController: UIViewController {
         for cookie in HttpClient().getCookies() {
             webView.configuration.websiteDataStore.httpCookieStore.setCookie(cookie, completionHandler: nil)
         }
+        let MockWebViewUserAgent = "kto-app-ios/\(UIDevice.current.systemVersion) APPv\(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "")"
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1 \(MockWebViewUserAgent)"
         
         if let urlString = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let urlHost = URL(string: urlString) {
             let request = URLRequest(url: urlHost)
