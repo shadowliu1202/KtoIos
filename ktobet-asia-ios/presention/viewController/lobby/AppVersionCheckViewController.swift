ca4b12aac89d73de7869bb092acbbd500e107c17
weichen Cheng <weichen.cheng@higgstar.com>
2022-01-27 14:56:29 +0800

    APP-2637 [IOS][Maintenance] 遊戲中遇維護 返回後表現需調整

--- ktobet-asia-ios/presention/viewController/lobby/AppVersionCheckViewController.swift
+++ ktobet-asia-ios/presention/viewController/lobby/AppVersionCheckViewController.swift
@@ -1,18 +1,23 @@
 import UIKit
 import RxSwift
+import SharedBu
 
 class AppVersionCheckViewController: APPViewController {
     private var appSyncDispose: Disposable?
+    private var disposeBag = DisposeBag()
+    private var serviceViewModel = DI.resolve(ServiceStatusViewModel.self)!
+    private let playerViewModel = DI.resolve(PlayerViewModel.self)!
+    private var productType: ProductType?
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         observerCompulsoryUpdate()
+        observeSystemStatus()
     }
     
     func observerCompulsoryUpdate() {
-        let playerViewModel = DI.resolve(PlayerViewModel.self)!
-        appSyncDispose = AppSynchronizeViewModel.shared.compulsoryupdate.compactMap({$0}).subscribe(onNext: { _ in
-            let _ = playerViewModel.logout().subscribeOn(MainScheduler.instance).subscribe(onCompleted: {
+        appSyncDispose = AppSynchronizeViewModel.shared.compulsoryupdate.compactMap({$0}).subscribe(onNext: {[weak self] _ in
+            let _ = self?.playerViewModel.logout().subscribeOn(MainScheduler.instance).subscribe(onCompleted: {
                 NavigationManagement.sharedInstance.goTo(storyboard: "Login", viewControllerId: "LoginNavigation")
             }, onError: {
                 print($0)
@@ -20,17 +25,44 @@ class AppVersionCheckViewController: APPViewController {
         })
     }
     
+    private func observeSystemStatus() {
+        serviceViewModel.output.portalMaintenanceStatus.drive(onNext: { [weak self] status in
+            guard let self = self, let productType = self.productType else { return }
+            switch status {
+            case is MaintenanceStatus.AllPortal:
+                self.playerViewModel.logout()
+                    .subscribeOn(MainScheduler.instance)
+                    .subscribe(onCompleted: { [weak self] in
+                        self?.showLoginMaintenanAlert()
+                    }).disposed(by: self.disposeBag)
+            case let productStatus as MaintenanceStatus.Product:
+                if productStatus.isProductMaintain(productType: productType) {
+                    NavigationManagement.sharedInstance.goTo(productType: productType, isMaintenance: true)
+                }
+            default:
+                break
+            }
+        }).disposed(by: disposeBag)
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         appSyncDispose?.dispose()
     }
+    
+    deinit {
+        print("\(type(of: self)) deinit")
+    }
 }
 
 extension AppVersionCheckViewController: WebGameViewCallback {
-    func gameDidDisappear() {
+    func gameDidDisappear(productType: ProductType?) {
         if Reachability?.isNetworkConnected == false {
             self.networkDisConnected()
         }
         AppSynchronizeViewModel.shared.syncAppVersion()
+        
+        self.productType = productType
+        serviceViewModel.refreshProductStatus()
     }
 }
