555e187322f42c4846bfad18d90c89a19026ac45
weichen Cheng <weichen.cheng@higgstar.com>
2022-03-25 16:41:01 +0800

    APP-2851 [iOS][Portal][Withdrawal] 選擇 陝西省 時，延安市顯示出 China 字樣 fix APP-2851 #comment root cause: wording issue \\ solution: fix wording update sharedbu version

--- ktobet-asia-ios/presention/viewController/AppSynchronizeViewModel.swift
+++ ktobet-asia-ios/presention/viewController/AppSynchronizeViewModel.swift
@@ -11,7 +11,7 @@ class AppSynchronizeViewModel {
     private var appUpdateUseCase: AppVersionUpdateUseCase!
     var compulsoryupdate = BehaviorRelay<Version?>(value: nil)
     var optionalupdate = BehaviorRelay<Version?>(value: nil)
-    var uptodate = BehaviorRelay<Version.VersionState?>(value: nil)
+    var uptodate = BehaviorRelay<Version.UpdateAction?>(value: nil)
     lazy var synchronize = self.appUpdateUseCase.synchronize()
     func getCurrentVersion() -> Version {
         Version.companion.create(version: Bundle.main.releaseVersionNumber)
@@ -21,7 +21,7 @@ class AppSynchronizeViewModel {
         guard Configuration.isAutoUpdate else { return }
         let current = getCurrentVersion()
         let _ = appUpdateUseCase.synchronize().subscribe(onSuccess: { [unowned self] in
-            let state = $0.getVersionState(currentVersion: current)
+            let state = current.getUpdateAction(latestVersion: $0)
             switch state {
             case .compulsoryupdate:
                 self.compulsoryupdate.accept($0)
