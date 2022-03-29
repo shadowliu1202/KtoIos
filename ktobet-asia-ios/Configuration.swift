5f3959ff3f5e7289e86bd400db92f407af6c2248
Weichen Cheng <weichen.cheng@higgstar.com>
2022-03-29 15:55:47 +0800

    APP-2730 [iOS][備用域名] 當主域名失效時更新備用域名

--- ktobet-asia-ios/Configuration.swift
+++ ktobet-asia-ios/Configuration.swift
@@ -6,6 +6,7 @@ enum Configuration: String {
     case dev
     case qat
     case staging
+    case production
     
     static private let current: Configuration = {
 #if DEV
@@ -14,6 +15,8 @@ enum Configuration: String {
         return .qat
 #elseif STAGING
         return .staging
+#elseif PRODUCTION
+        return .production
 #endif
     }()
     static private let env: Env = {
@@ -24,17 +27,53 @@ enum Configuration: String {
             return QatConfig()
         case .staging:
             return StagingConfig()
+        case .production:
+            return ProductionConfig()
         }
     }()
     
-    static var host: String         = "https://\(env.hostName)/"
-    static var hostName: String     = env.hostName
+    static var host: String         = "https://\(hostName)/"
+    static var hostName: String     = env.hostName.first(where: checkNetwork) ?? env.hostName.first!
     static var disableSSL: Bool     = env.disableSSL
     static var isAutoUpdate: Bool   = env.isAutoUpdate
     static var manualUpdate: Bool   = env.manualUpdate
     static var debugGesture: Bool   = env.debugGesture
     static var affiliateUrl: URL    = URL(string: "\(host)affiliate")!
-    
+
+    static private func checkNetwork(url: String) -> Bool {
+        let group = DispatchGroup()
+        group.enter()
+        var isSuccess = false
+        guard let url = URL(string: "https://\(url)") else {
+            group.leave()
+            return isSuccess
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "HEAD"
+        URLSession(configuration: .default)
+            .dataTask(with: request) { (_, response, error) -> Void in
+            guard error == nil else {
+                print("Error:", error ?? "")
+                isSuccess = false
+                group.leave()
+                return
+            }
+
+            guard (response as? HTTPURLResponse)?
+                .statusCode == 200 else {
+                isSuccess = false
+                group.leave()
+                return
+            }
+
+            isSuccess = true
+            group.leave()
+        }.resume()
+
+        group.wait()
+        return isSuccess
+    }
     
     static func getKtoAgent() -> String {
         let userAgent = "kto-app-ios/\(UIDevice.current.systemVersion) APPv\(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "")"
@@ -43,7 +82,7 @@ enum Configuration: String {
 }
 
 protocol Env {
-    var hostName: String { get }
+    var hostName: [String] { get }
     var disableSSL: Bool { get }
     var isAutoUpdate: Bool { get }
     var manualUpdate: Bool { get }
@@ -51,7 +90,7 @@ protocol Env {
 }
 
 fileprivate class DevConfig: Env {
-    var hostName: String = "qat1-mobile.affclub.xyz"
+    var hostName: [String] = ["qat1-mobile.affclub.xyz"]
     var disableSSL: Bool = true
     var isAutoUpdate: Bool = false
     var manualUpdate: Bool = true
@@ -59,7 +98,7 @@ fileprivate class DevConfig: Env {
 }
 
 fileprivate class QatConfig: Env {
-    var hostName: String = "qat1-mobile.affclub.xyz"
+    var hostName: [String] = ["qat1-mobile.affclub.xyz", "qat1-mobile2.affclub.xyz"]
     var disableSSL: Bool = true
     var isAutoUpdate: Bool = false
     var manualUpdate: Bool = true
@@ -67,7 +106,15 @@ fileprivate class QatConfig: Env {
 }
 
 fileprivate class StagingConfig: Env {
-    var hostName: String = "mobile.staging.support"
+    var hostName: [String] = ["mobile.staging.support", "mobile2.staging.support"]
+    var disableSSL: Bool = false
+    var isAutoUpdate: Bool = true
+    var manualUpdate: Bool = false
+    var debugGesture: Bool = false
+}
+
+fileprivate class ProductionConfig: Env {
+    var hostName: [String] = ["appkto.com", "hekto.app"]
     var disableSSL: Bool = false
     var isAutoUpdate: Bool = true
     var manualUpdate: Bool = false
