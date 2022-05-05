08113d918aef47c6a4cb9e7261cd9b79521c2411
Neil Liu <neil.liu@higgstar.com>
2022-05-05 18:13:37 +0800

    ci:update release

--- pipeline/production/publish.groovy
+++ pipeline/production/publish.groovy
@@ -1,4 +1,3 @@
-def remote = [:]
 pipeline {
     agent {
         label 'master'
@@ -8,71 +7,86 @@ pipeline {
     }
     environment {
         SYSADMIN_RSA = '0dd067b6-8bd0-4c0a-9cb7-fb374ed7084e'
-        TARGET_TAG = "$params.RELEASE_TAG"
-        VERSION_CORE = "${params.RELEASE_TAG.split('\\+')[0].split('-')[0]}"
+        TARGET_TAG = "$params.PARAMS_SELECT_TAG"
+        PROP_VERSION_CORE = "${params.PARAMS_SELECT_TAG.split('\\+')[0].split('-')[0]}"
         //CHANGE
         REMOTE_ANS_NAME = 'ansible-server'
-        PROP_BUILD_ENVIRONMENT = "${env.BUILD_ENVIRONMENT}"
-        PROP_REMOTE_ANS_HOST = "${env.REMOTE_ANS_HOST}"
-        PROP_DOWNSTREAM_JIRA_JOB = "${env.JIRA_JOB}"
-        PROP_APP_DOWNLOAD_LINK = "${env.APP_DOWNLOAD_LINK }"
+        PROP_BUILD_ENVIRONMENT = "$env.BUILD_ENVIRONMENT"
+        PROP_REMOTE_ANS_HOST = "$env.REMOTE_ANS_HOST"
+        PROP_DOWNSTREAM_JIRA_JOB = "$env.JIRA_JOB"
+        PROP_APP_DOWNLOAD_LINK = "$env.APP_DOWNLOAD_LINK"
+        PROP_TEAMS_NOTIFICATION = "$env.TEAMS_NOTIFICATION_TOKEN"
     }
     stages {
         stage('Publish APK') {
             steps {
                 cleanWs()
-                withEnv(["PublishTag=${params.RELEASE_TAG}"]) {
+                withEnv(["PublishTag=${params.PARAMS_SELECT_TAG}"]) {
                     script {
+                        def remote = [:]
                         remote.name = "$REMOTE_ANS_NAME"
                         remote.host = "$PROP_REMOTE_ANS_HOST"
                         remote.allowAnyHosts = true
-                        currentBuild.displayName = "[${PROP_BUILD_ENVIRONMENT}] ${PublishTag}"
+                        currentBuild.displayName = "[${PROP_BUILD_ENVIRONMENT}] $PublishTag"
                         withCredentials([sshUserPrivateKey(credentialsId: "$SYSADMIN_RSA", keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'user')]) {
                             remote.identityFile = identity
                             remote.user = user
+                            // Get Production version
+                            def commandResult = sshCommand remote: remote, command: "curl -s https://appkto.com/android/api/get-android-apk-version | jq -r '.data.apkVersion'"
+                            echo "$commandResult"
+                            String[] result = commandResult.trim().split('\\+')
+                            if(result.length == 1) {
+                                env.PRODUCTION_ONLINE_TAG = "${result[0]}-release"
+                            }else{
+                             env.PRODUCTION_ONLINE_TAG = "${result[0]}-release+${result[1]}"
+                            }
+                            echo "production version = $PRODUCTION_ONLINE_TAG"
+                            // Get Online version
                             writeFile file: 'version', text: ''
                             sshGet remote: remote, from: '/data-disk/mobile-deployment-document/android.version', into: 'version', override: true
                             env.CURRENT_ONLINE_TAG = readFile('version').trim()
                             echo "$PROP_BUILD_ENVIRONMENT version = $CURRENT_ONLINE_TAG"
-                            sshCommand remote: remote, command: """
-                                ansible-playbook -v /data-disk/brand-deployment-document/playbooks/deploy-kto-android-apk.yml --extra-vars "tag=$PublishTag"
-                                echo $PublishTag > /data-disk/mobile-deployment-document/android.version
-                            """
+                            // sshCommand remote: remote, command: """
+                            //     ansible-playbook -v /data-disk/brand-deployment-document/playbooks/deploy-kto-android-apk.yml --extra-vars "tag=$PublishTag"
+                            //     echo $PublishTag > /data-disk/mobile-deployment-document/android.version
+                            // """
                         }
                     }
+                    echo sh(script: 'env|sort', returnStdout: true)
                 }
             }
         }
 
         stage('Update jira issues') {
             steps {
-                withEnv(["JOB=$PROP_DOWNSTREAM_JIRA_JOB",
-                         "TARGET=$TARGET_TAG",
-                         "CURRENT=$CURRENT_ONLINE_TAG",
-                         "ENV=${PROP_BUILD_ENVIRONMENT.toLowerCase()}"
+                withEnv(["DownStreamJob=$env.PROP_DOWNSTREAM_JIRA_JOB",
+                         "Target=$params.PARAMS_SELECT_TAG",
+                         "Current=$env.PRODUCTION_ONLINE_TAG",
                 ]) {
-                    build job: "$PROP_DOWNSTREAM_JIRA_JOB", parameters: [text(name: 'TARGET_TAG', value: "$TARGET_TAG"),
-                                                                         text(name: 'CURRENT_ONLINE_TAG', value: "$CURRENT"),
-                                                                         text(name: 'ENV', value: "$ENV")]
+                    echo "trigger job $DownStreamJob"
+                    build job: "$DownStreamJob" , parameters: [text(name: 'TARGET_TAG', value: "$Target"),text(name: 'PRODUCTION_TAG', value: "$Current")]
                 }
             }
         }
 
-        stage('Notification') {
+        stage('Publish Notification') {
             steps {
                 script {
-                    withEnv(["PUBLISH_TAG=${params.RELEASE_TAG}",
-                             "ENVIRONMENT=${PROP_BUILD_ENVIRONMENT.toLowerCase()}",
-                             "VERSION=$VERSION_CORE",
-                             "TEAMS_TOKEN=${env.TEAMS_NOTIFICATION}",
-                             "DownloadLink=$PROP_APP_DOWNLOAD_LINK"
+                    withEnv(["PublishTag=$params.PARAMS_SELECT_TAG",
+                             "OnlineTag=$env.CURRENT_ONLINE_TAG",
+                             "Enviroment=${PROP_BUILD_ENVIRONMENT.toLowerCase()}",
+                             "Version=$PROP_VERSION_CORE",
+                             "TeamsToken=$PROP_TEAMS_NOTIFICATION",
+                             "DownloadLink=$PROP_APP_DOWNLOAD_LINK",
+                             "UpdateIssues=https://jira.higgstar.com/issues/?jql=project = APP AND labels = android-$PROP_VERSION_CORE-${PROP_BUILD_ENVIRONMENT.toLowerCase()}",
                     ]) {
-                        String path = PUBLISH_TAG.split('\\+')[0]
-
-                        office365ConnectorSend webhookUrl: "$TEAMS_NOTIFICATION",
-                                message: ">**[Android] [KTO Asia]** has been deployed to $ENVIRONMENT</br>version : **[$PUBLISH_TAG]($PROGET_HOME/feeds/app/android/kto-asia/${path}/files)**",
-                                factDefinitions: [[name: 'Download Page', template: "<a href=\"$DownloadLink\">Download Page</a>"],
-                                                  [name: 'Related Issues', template: "<a href=\"https://jira.higgstar.com/issues/?jql=project = APP AND labels = android-$VERSION-$ENVIRONMENT\">Jira Issues</a>"]]
+                        String path = PublishTag.split('\\+')[0]
+                        String publish = PublishTag.split('-')[0]
+                        String online = OnlineTag.split('-')[0]
+                        office365ConnectorSend webhookUrl: "$TeamsToken",
+                                message: ">**[Android] [KTO Asia]** has been published to $Enviroment</br>version : **[$PublishTag]($JENKINS_PROGET_HOME/feeds/app/android/kto-asia/${path}/files)**",
+                                factDefinitions: [[name: 'Download Page', template: "<a href=\"$DownloadLink\">link</a>"],
+                                                  [name: 'Update Issues', template: "$online->$publish (<a href=\"$UpdateIssues\">issues</a>)"]]
                     }
                 }
             }
