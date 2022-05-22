8e11026e29f5d08809521d328b68fb742dd4940a
neil <neil.liu@higgstar.com>
2022-05-22 14:49:48 +0800

    ci

--- pipeline/library/vars/teams.groovy
+++ pipeline/library/vars/teams.groovy
@@ -3,7 +3,7 @@ def notifyQat3(def teamsToken, def versionCore, def preRelease, def newBuildNumb
     def updateIssues = "https://jira.higgstar.com/issues/?jql=project = APP AND labels = ios-$versionCore-$preRelease"
     def releasePath = "$versionCore-$preRelease"
     wrap([$class: 'BuildUser']) {
-        office365ConnectorSend webhookUrl: "$TeamsToken",
+        office365ConnectorSend webhookUrl: "$teamsToken",
                                 message: ">**[IOS][Hotfix][KTO Asia]** has been deployed to QAT3</br>version : **[$tag]($JENKINS_PROGET_HOME/feeds/app/ios/kto-asia/$releasePath/files)**",
                                 factDefinitions: [[name: 'testflight', template: "<a href=\"$testFlightLink\">Download Page</a>"],
                                                   [name: 'hotfix issues', template: "[$versionCore] $oldBuildNumber to $newBuildNumber (<a href=\"$updateIssues\">issues</a>)"],
