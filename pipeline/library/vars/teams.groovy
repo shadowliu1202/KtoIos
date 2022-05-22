841df7b6dfac0bbf297a535f59370c1039690231
neil <neil.liu@higgstar.com>
2022-05-22 15:06:27 +0800

    ci

--- pipeline/library/vars/teams.groovy
+++ pipeline/library/vars/teams.groovy
@@ -6,8 +6,8 @@ def notifyQat3(def teamsTokenId, def versionCore, def preRelease, def newBuildNu
         withCredentials([string(credentialsId: "$teamsTokenId", variable: 'token')]) {
            office365ConnectorSend webhookUrl: "$token",
                                 message: ">**[IOS][Hotfix][KTO Asia]** has been deployed to QAT3</br>version : **[$tag]($JENKINS_PROGET_HOME/feeds/app/ios/kto-asia/$releasePath/files)**",
-                                factDefinitions: [[name: 'testflight', template: "<a href=\"$testFlightLink\">Download Page</a>"],
-                                                  [name: 'hotfix issues', template: "[$versionCore] $oldBuildNumber to $newBuildNumber (<a href=\"$updateIssues\">issues</a>)"],
+                                factDefinitions: [[name: 'testflight page', template: "<a href=\"$testFlightLink\">link</a>"],
+                                                  [name: 'hotfix issues', template: "[$versionCore] update from $oldBuildNumber to $newBuildNumber (<a href=\"$updateIssues\">issues</a>)"],
                                                   [name: 'release by', template: "${env.BUIlD_USER}"]]
         }
 
