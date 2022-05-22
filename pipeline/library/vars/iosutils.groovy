b94f87f385d68c705d9a190ede9aec5aa6a74a72
neil <neil.liu@higgstar.com>
2022-05-22 23:31:57 +0800

    ci

--- pipeline/library/vars/iosutils.groovy
+++ pipeline/library/vars/iosutils.groovy
@@ -35,7 +35,7 @@ def getTestFlightBuildNumber(versionCore, enviroment) {
 }
 
 def getNextBuildNumber(productionTag, versionCore, enviroment) {
-     withEnv(['MATCH_PASSWORD=password']) {
+     withEnv(['MATCH_PASSWORD=password', 'KEY_ID=2XHCS3W99M']) {
         withCredentials([file(credentialsId: '63f71ab5-5473-43ca-9191-b34cd19f1fa1', variable: 'API_KEY'),
                     string(credentialsId: 'ios_agent_keychain_password', variable: 'KEYCHAIN_PASSWORD')
         ]) {
