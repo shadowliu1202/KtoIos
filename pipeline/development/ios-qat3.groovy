f9348c17cb73097298eef6bc1417628a8c680ac0
neil <neil.liu@higgstar.com>
2022-05-22 12:31:14 +0800

    CI

--- pipeline/development/ios-qat3.groovy
+++ pipeline/development/ios-qat3.groovy
@@ -122,7 +122,7 @@ pipeline {
                                 echo "Get Ipa Size = $IPA_SIZE"
                             }
                         }
-                        uploadProgetPackage artifacts: 'output/*.ipa', feedName: 'app', groupName: 'ios', packageName: 'kto-asia', version: "$env.PROP_RELEASE_TAG", description: "compile version:$env.PROP_NEXT_BUILD_NUMBER"
+                        uploadProgetPackage artifacts: 'output/*.ipa', feedName: 'app', groupName: 'ios', packageName: 'kto-asia', version: "$env.RELEASE_VERSION", description: "compile version:$env.PROP_NEXT_BUILD_NUMBER"
                     }
                 }
             }
@@ -141,31 +141,28 @@ pipeline {
                          "BuildEnviroment=$PROP_BUILD_ENVIRONMENT",
                          "DownloadLink=$PROP_DOWNLOAD_LINK"
                 ]) {
-                    withCredentials([sshUserPrivateKey(credentialsId: "$RootCredentialsId", keyFileVariable: 'keyFile', passphraseVariable: '', usernameVariable: 'username')]) {
-                        script {
-                            def 
-                            def remote = [:]
-                            remote.name = 'mis ansible'
-                            remote.host = 'mis-ansible-app-01p'
-                            remote.user = 'root'
-                            remote.identityFile = keyFile
-                            remote.allowAnyHosts = true
-
-                            sshCommand remote: remote, command: """
-                                ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_TIMEOUT=30; ansible-playbook -v /data-disk/brand-team/deploy-kto-ios-ipa.yml -u root --extra-vars "apkFeed=kto-asia tag=$HotfixVersion ipa_size=$IpaSize download_url=$DownloadLink" -i /data-disk/brand-team/qat3.ini
-                            """
-                        }
-                    }
                     dir('project') {
-                        wrap([$class: 'BuildUser']) {
-                            sshagent(["$JenkinsCredentialsId"]) {
+                        withCredentials([sshUserPrivateKey(credentialsId: "$RootCredentialsId", keyFileVariable: 'keyFile', passphraseVariable: '', usernameVariable: 'username')]) {
+                            script {
+                                def remote = [:]
+                                remote.name = 'mis ansible'
+                                remote.host = 'mis-ansible-app-01p'
+                                remote.user = 'root'
+                                remote.identityFile = keyFile
+                                remote.allowAnyHosts = true
+
+                                sshCommand remote: remote, command: """
+                                    ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_TIMEOUT=30; ansible-playbook -v /data-disk/brand-team/deploy-kto-ios-ipa.yml -u root --extra-vars "apkFeed=kto-asia tag=$HotfixVersion ipa_size=$IpaSize download_url=$DownloadLink" -i /data-disk/brand-team/qat3.ini
+                                """
+                            }
+                        }
+                        sshagent(["$JenkinsCredentialsId"]) {
                                 sh script:"""
                                     git config user.name "devops"
                                     git tag -f -a -m "release $BuildEnviroment version from ${env.BUIlD_USER}" $ReleaseTag
                                     git push $PROP_GIT_REPO_URL $ReleaseTag
                                 """ , returnStatus:true
                             }
-                        }
                     }
                 }
             }
