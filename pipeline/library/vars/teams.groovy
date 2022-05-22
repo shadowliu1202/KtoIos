2496e32177f2e12e9f9d6b091a7561d7325cbba7
neil <neil.liu@higgstar.com>
2022-05-22 15:03:46 +0800

    ci

--- pipeline/library/vars/teams.groovy
+++ pipeline/library/vars/teams.groovy
@@ -1,13 +1,15 @@
-def notifyQat3(def teamsToken, def versionCore, def preRelease, def newBuildNumber,def oldBuildNumber, def testFlightLink) {
+def notifyQat3(def teamsTokenId, def versionCore, def preRelease, def newBuildNumber,def oldBuildNumber, def testFlightLink) {
     def tag = "$versionCore-$preRelease+$newBuildNumber"
     def updateIssues = "https://jira.higgstar.com/issues/?jql=project = APP AND labels = ios-$versionCore-$preRelease"
     def releasePath = "$versionCore-$preRelease"
-    echo "get $teamsToken"
     wrap([$class: 'BuildUser']) {
-        office365ConnectorSend webhookUrl: "$teamsToken",
+        withCredentials([string(credentialsId: "$teamsTokenId", variable: 'token')]) {
+           office365ConnectorSend webhookUrl: "$token",
                                 message: ">**[IOS][Hotfix][KTO Asia]** has been deployed to QAT3</br>version : **[$tag]($JENKINS_PROGET_HOME/feeds/app/ios/kto-asia/$releasePath/files)**",
                                 factDefinitions: [[name: 'testflight', template: "<a href=\"$testFlightLink\">Download Page</a>"],
                                                   [name: 'hotfix issues', template: "[$versionCore] $oldBuildNumber to $newBuildNumber (<a href=\"$updateIssues\">issues</a>)"],
                                                   [name: 'release by', template: "${env.BUIlD_USER}"]]
+        }
+
     }
 }
