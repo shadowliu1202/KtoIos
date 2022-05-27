8879307ffd57ebdeda33158036893bb29df767dd
neil <neil.liu@higgstar.com>
2022-05-27 11:27:55 +0800

    ci

--- pipeline/library/vars/version.groovy
+++ pipeline/library/vars/version.groovy
@@ -9,7 +9,7 @@ def getReleaseTag(core, preRelease, buildNumber) {
 }
 
 def setIosTag(core, preRelease, buildNumber, buildEnviroment) {
-    def releaseTag = getReleaseTag(core, preRelease, buildEnviroment)
+    def releaseTag = getReleaseTag(core, preRelease, buildNumber)
     string gitRepo = 'git@gitlab.higgstar.com:mobile/ktobet-asia-ios.git'
     def gitCredentialsId = '28ef89bf-70a2-475d-b5e0-a1ea12a8fcdb'
     wrap([$class: 'BuildUser']) {
