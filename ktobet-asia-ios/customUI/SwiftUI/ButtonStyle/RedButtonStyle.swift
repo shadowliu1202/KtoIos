13b7314c59988bffd3165c59e51783fd44cf25eb
Leo Hsu <leo.hsu@higgstar.com>
2022-07-15 19:27:17 +0800

    fix: APP-3338 [iOS]修正iOS14 StarMergerView文字描述沒有完全展開

--- ktobet-asia-ios/customUI/SwiftUI/ButtonStyle/RedButtonStyle.swift
+++ ktobet-asia-ios/customUI/SwiftUI/ButtonStyle/RedButtonStyle.swift
@@ -3,15 +3,17 @@ import SwiftUI
 struct RedButtonStyle: ButtonStyle {
     @Environment(\.isEnabled) var isEnabled
     func makeBody(configuration: Self.Configuration) -> some View {
-        configuration.label.foregroundColor(isEnabled ? .white : .white40)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        configuration.label
+            .foregroundColor(isEnabled ? .white : .white40)
             .font(.custom("PingFangSC-Regular", size: 14))
             .padding(10)
+            .frame(maxWidth: .infinity)
             .lineLimit(1)
-            .background(RoundedRectangle(cornerRadius: 8)
-                            .frame(height: 48)
-                            .foregroundColor(isEnabled ? .primaryRed : .primaryRed30))
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .frame(height: 48)
+                    .foregroundColor(isEnabled ? .primaryRed : .primaryRed30)
+            )
             .opacity(configuration.isPressed ? 0.8 : 1.0)
-            .scaleEffect(configuration.isPressed ? 0.98 : 1.0)
     }
 }
