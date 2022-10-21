e08bb59ce5192589a9f7f27523a91d44bb4034fa
Leo Hsu <leo.hsu@higgstar.com>
2022-10-21 19:36:12 +0800

    fix: KTO-1589 UiTextFeild金額不允許貼上 fix: KTO-1590 SwiftuiInputText實作輸入限制 fix: KTO-1553 [APP][iOS]調整帳號相關的欄位，複製貼上全形數字要能轉換成半形數字

---
+++ ktobet-asia-ios/customUI/PasteableTextField.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class PasteableTextField: UITextField {
+    @IBInspectable
+    public var disablePaste: Bool = false
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(UIResponderStandardEditActions.paste(_:)) {
+            return !disablePaste
+        }
+        
+        if #available(iOS 15.0, *) {
+            if action == #selector(UIResponder.captureTextFromCamera(_:)) {
+                return !disablePaste
+            }
+        }
+        
+        return super.canPerformAction(action, withSender: sender)
+    }
+}
