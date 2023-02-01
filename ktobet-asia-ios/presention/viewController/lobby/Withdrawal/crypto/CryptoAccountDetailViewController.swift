35d8ea1d66a978a5a4295d840dd949da0aa782d2
Finn Wu <finn.wu@higgstar.com>
2023-02-01 17:02:50 +0800

    fix: KTO-3368 [iOS] [Withdrawal] 刪除尚未驗證的虛擬幣帳戶時跳的Dialog Content 錯誤

--- ktobet-asia-ios/presention/viewController/lobby/Withdrawal/crypto/CryptoAccountDetailViewController.swift
+++ ktobet-asia-ios/presention/viewController/lobby/Withdrawal/crypto/CryptoAccountDetailViewController.swift
@@ -39,7 +39,7 @@ class CryptoAccountDetailViewController: LobbyViewController {
     }
     
     private func alertDelete() {
-        Alert.shared.show(Localize.string("withdrawal_bankcard_delete_confirm_title"), Localize.string("withdrawal_bankaccountdetail_modal_content"), confirm: {
+        Alert.shared.show(Localize.string("withdrawal_bankcard_delete_confirm_title"), Localize.string("withdrawal_bankcard_delete_confirm_content"), confirm: {
             self.deleteAccount()
         }, cancel: { }, tintColor: UIColor.redF20000)
     }
