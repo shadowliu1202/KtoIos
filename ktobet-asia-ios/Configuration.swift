863f9056b10daefcb0865c1d2b4f9948d0d1d73f
ian.chang <ian.chang@higgstar.com>
2023-04-26 14:33:14 +0800

    feat: KTO-4548 [iOS]在充值/提現/註冊/登入/投注等流程記錄會員裝置資訊

--- ktobet-asia-ios/Configuration.swift
+++ ktobet-asia-ios/Configuration.swift
@@ -61,7 +61,7 @@ enum Configuration: String {
 
   static func getKtoAgent() -> String {
     let userAgent =
-      "kto-app-ios/\(UIDevice.current.systemVersion) APPv\(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "") (\(UIDevice.modelName))"
+      "kto-app-ios/\(UIDevice.current.systemVersion) APPv\(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "") (\(UIDevice.deviceName))"
     return userAgent
   }
 }
