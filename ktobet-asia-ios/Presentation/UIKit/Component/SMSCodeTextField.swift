3c16d1f03b46d64d114738a36597974fbbbe7a4f
Ian Chang <ian.chang@higgstar.com>
2023-08-22 16:04:35 +0800

    refactor: KTO-5328 [iOS] Verification Style 調整一致

--- ktobet-asia-ios/Presentation/UIKit/Component/SMSCodeTextField.swift
+++ ktobet-asia-ios/Presentation/UIKit/Component/SMSCodeTextField.swift
@@ -7,10 +7,57 @@ protocol SMSCodeTextFieldDelegate: AnyObject {
 
 class SMSCodeTextField: UITextField {
   weak var myDelegate: SMSCodeTextFieldDelegate?
-  private var emptyCount = 0
-
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    delegate = self
+    initUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    delegate = self
+    initUI()
+  }
+  
   override func deleteBackward() {
     super.deleteBackward()
     myDelegate?.textFieldDidDelete(self)
+  }
+  
+  private func initUI() {
+    textContentType = .oneTimeCode
+    keyboardType = .numberPad
+    textAlignment = .center
+    textColor = .white
+    tintColor = .clear
+    backgroundColor = .inputDefault
+    layer.cornerRadius = 6
+    cornerRadius = 6
+    layer.masksToBounds = true
+    font = UIFont(name: "PingFangSC-Semibold", size: 18)
+    
+    translatesAutoresizingMaskIntoConstraints = false
+    widthAnchor.constraint(equalToConstant: 40).isActive = true
+    heightAnchor.constraint(equalToConstant: 40).isActive = true
+  }
+}
+
+extension SMSCodeTextField: UITextFieldDelegate {
+  func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+    textField.backgroundColor = .inputFocus
+    return true
+  }
+  
+  func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    textField.backgroundColor = .inputDefault
+    return true
+  }
+  
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    let newPosition = textField.endOfDocument
+    DispatchQueue.main.async {
+      textField.selectedTextRange = textField.textRange(from: newPosition, to: newPosition)
+    }
   }
 }
--- ktobet-asia-ios/Presentation/UIKit/Component/SMSVerifyCodeInputView.swift
+++ ktobet-asia-ios/Presentation/UIKit/Component/SMSVerifyCodeInputView.swift
@@ -1,14 +1,13 @@
 import Foundation
 import RxCocoa
+import SnapKit
 import UIKit
 
 class SMSVerifyCodeInputView: UIView {
-  @IBOutlet private var btnStackView: UIStackView!
   @IBOutlet private var codeStackView: UIStackView!
 
   private var otpLength = 0
-  private var codes: [UIView] = []
-  private var btns: [UIButton] = []
+  private var codes: [SMSCodeTextField] = []
 
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -16,93 +15,46 @@ class SMSVerifyCodeInputView: UIView {
 
   func setOtpMaxLength(otpLength: Int32) {
     self.otpLength = Int(otpLength)
-    initialize(otpLength)
-  }
-
-  func getOtpCode() -> Observable<String> {
-    let codesOb = codes
-      .compactMap { view -> ControlProperty<String>? in
-        guard let textField = view.subviews.first as? SMSCodeTextField
-        else { return nil }
-
-        return textField.rx.text.orEmpty
+    
+    let isVN = Int(otpLength) == OTPVerifyTextField.VNMobileLength
+    codeStackView.spacing = isVN ? 12 : 0
+    if !isVN {
+      codeStackView.snp.makeConstraints { make in
+        make.edges.equalToSuperview()
       }
-
-    return Observable.combineLatest(codesOb).map { stringElement -> String in
-      stringElement.reduce("") { $0 + $1 }
     }
-  }
-
-  private func initialize(_ otpLength: Int32) {
+    
     for index in 0..<Int(otpLength) {
-      let backgroundView = UIView()
       let code = SMSCodeTextField()
-      let btn = UIButton()
-
-      backgroundView.backgroundColor = .white.withAlphaComponent(0.15)
-      backgroundView.cornerRadius = 6
-      backgroundView.widthAnchor.constraint(equalToConstant: 40).isActive = true
-      backgroundView.heightAnchor.constraint(equalToConstant: 40).isActive = true
-
-      backgroundView.addSubview(code)
 
       code.myDelegate = self
-      code.textContentType = .oneTimeCode
-      code.keyboardType = .numberPad
-      code.textAlignment = .left
-      code.textColor = .white
-      code.tintColor = .primaryDefault
-      code.backgroundColor = .clear
-      code.layer.cornerRadius = 6
-      code.layer.masksToBounds = true
       code.tag = index
-      code.font = UIFont(name: "PingFangSC-Semibold", size: 18)
       code.addTarget(self, action: #selector(textEditingChaged(_:)), for: .editingChanged)
-
-      guard let codeSuperView = code.superview else { return }
-
-      code.translatesAutoresizingMaskIntoConstraints = false
-      code.leadingAnchor.constraint(equalTo: codeSuperView.leadingAnchor, constant: 14).isActive = true
-      code.trailingAnchor.constraint(equalTo: codeSuperView.trailingAnchor).isActive = true
-      code.topAnchor.constraint(equalTo: codeSuperView.topAnchor).isActive = true
-      code.bottomAnchor.constraint(equalTo: codeSuperView.bottomAnchor).isActive = true
-
-      btn.translatesAutoresizingMaskIntoConstraints = false
-      btn.widthAnchor.constraint(equalToConstant: 40).isActive = true
-      btn.heightAnchor.constraint(equalToConstant: 40).isActive = true
-      btn.addTarget(self, action: #selector(btnCodePressed(_:)), for: .touchUpInside)
-      btn.tag = index
-      codes.append(backgroundView)
-      btns.append(btn)
-      codeStackView.insertArrangedSubview(backgroundView, at: index)
-      btnStackView.insertArrangedSubview(btn, at: index)
+      
+      codes.append(code)
+      codeStackView.insertArrangedSubview(code, at: index)
     }
 
     backgroundColor = .clear
   }
+  
+  func getOtpCode() -> Observable<String> {
+    let codeObs = codes.map { $0.rx.text.orEmpty }
 
-  @objc
-  private func btnCodePressed(_ sender: UIButton) {
-    guard let codeTextField = codes[sender.tag].subviews.first as? SMSCodeTextField
-    else { return }
-
-    codeTextField.becomeFirstResponder()
+    return Observable.combineLatest(codeObs)
+      .map { stringElement -> String in
+        stringElement.reduce("") { $0 + $1 }
+      }
   }
 
   @objc
   private func textEditingChaged(_ sender: UITextField) {
     if let text = sender.text, text.count >= 1, text.count < otpLength {
-      sender.text = {
-        let index = text.index(text.endIndex, offsetBy: -1)
-        return String(text[index])
-      }()
+      guard let lastText = text.last else { return }
+      sender.text = String(lastText)
 
       let index = sender.tag + 1 > otpLength - 1 ? otpLength - 1 : sender.tag + 1
-
-      guard let codeTextField = codes[index].subviews.first as? SMSCodeTextField
-      else { return }
-
-      codeTextField.becomeFirstResponder()
+      codes[index].becomeFirstResponder()
     }
   }
 }
@@ -111,11 +63,7 @@ extension SMSVerifyCodeInputView: SMSCodeTextFieldDelegate, UITextFieldDelegate 
   func textFieldDidDelete(_ sender: SMSCodeTextField) {
     if (sender.text?.count ?? 0) == 0 {
       let index = sender.tag - 1 < 0 ? 0 : sender.tag - 1
-
-      guard let codeTextField = codes[index].subviews.first as? SMSCodeTextField
-      else { return }
-
-      codeTextField.becomeFirstResponder()
+      codes[index].becomeFirstResponder()
     }
   }
 }
