3c26d085e9c19d6515e8a42febde88d300bfca62
ian.chang <ian.chang@higgstar.com>
2023-09-13 15:17:29 +0800

    fix: KTO-6159 [iOS] swiftUI版本的textfield快速輸入會crash

--- ktobet-asia-ios/Presentation/UIKit/Component/PasteableTextField.swift
+++ ktobet-asia-ios/Presentation/UIKit/Component/PasteableTextField.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class PasteableTextField: UITextField {
   @IBInspectable public var disablePaste = false
+  
+  override var undoManager: UndoManager? { nil }
 
   override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
     if action == #selector(UIResponderStandardEditActions.paste(_:)) {
