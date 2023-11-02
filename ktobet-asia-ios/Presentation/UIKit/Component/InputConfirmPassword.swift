3e8cfa9ce6573a70758f51066ddd87880b6fb80a
Ian Chang <ian.chang@higgstar.com>
2023-11-02 15:27:28 +0800

    fix: KTO-1262 [iOS] InputText的錯誤樣式不正確

--- ktobet-asia-ios/Presentation/UIKit/Component/InputConfirmPassword.swift
+++ ktobet-asia-ios/Presentation/UIKit/Component/InputConfirmPassword.swift
@@ -37,6 +37,7 @@ class InputConfirmPassword: UIView {
     labSubTitle.backgroundColor = .clear
 
     textContent.textColor = .white
+    textContent.tintColor = .primaryDefault
     textContent.backgroundColor = .clear
     textContent.font = UIFont(name: "PingFangSC-Regular", size: 16)
     textContent.borderStyle = .none
@@ -70,12 +71,12 @@ class InputConfirmPassword: UIView {
       position = editedPosition()
     }
 
+    self.textContent.frame = position.content
     let changePosition = {
       self.labTitle.font = position.titleFont
       self.labTitle.frame = position.title
       self.labSubTitle.frame = position.subTitle
-      self.textContent.frame = position.content
-      self.underline.frame = CGRect(x: 0, y: self.bounds.maxY - 1, width: self.bounds.width, height: 1)
+      self.underline.frame = CGRect(x: 0, y: self.bounds.maxY - 2, width: self.bounds.width, height: 2)
       self.backgroundColor = self.getAllFocus() ? UIColor.inputFocus : UIColor.inputDefault
     }
     if firstPosition {
