0d9060fd0ab2f2b2dab7f1a017302e2cca061e9e
Ian Chang <ian.chang@higgstar.com>
2023-12-04 15:44:20 +0800

    fix: KTO-7115 [iOS] 優惠卷歷史篩選頁，點擊產品特有選項crash

---
+++ AIO-unitTests/PromotionPresenterTest.swift
@@ -0,0 +1,21 @@
+import Mockingbird
+import sharedbu
+import XCTest
+
+@testable import ktobet_asia_ios_qat
+
+final class PromotionPresenterTest: XCBaseTestCase {
+  func test_givenCNPlayer_thenDisplayVVIPOption_KTO_TC_25() {
+    let presenter = PromotionPresenter(supportLocale: .China())
+    let hasVVIPOption = presenter.conditions.contains(where: { $0.privilegeType == .vvipcashback })
+    
+    XCTAssertTrue(hasVVIPOption)
+  }
+  
+  func test_givenVNPlayer_thenNotDisplayVVIPOption_KTO_TC_953() {
+    let presenter = PromotionPresenter(supportLocale: .Vietnam())
+    let hasVVIPOption = presenter.conditions.contains(where: { $0.privilegeType == .vvipcashback })
+    
+    XCTAssertFalse(hasVVIPOption)
+  }
+}
